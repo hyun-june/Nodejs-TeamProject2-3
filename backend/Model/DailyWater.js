bccb3f34bf1484ba55d510979cc83efcdf1f75f3
Yongmin0423 <yym21345@gmail.com>
2024-11-14 17:07:40 +0900

    feat: Backend Setting

---
+++ backend/Model/DailyWater.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const dailyWaterSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    amount: { type: Number, required: true },
+    date: { type: Date, default: Date.now },
+  },
+  { timestamps: true }
+);
+
+export const DailyWater = mongoose.model("DailyWater", dailyWaterSchema);
--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -4,3 +4,4 @@ export const userRouter = express.Router();
 
 userRouter.route("/").get();
 userRouter.route("/:id").get().put();
+userRouter.route("/detail").get().post().put();

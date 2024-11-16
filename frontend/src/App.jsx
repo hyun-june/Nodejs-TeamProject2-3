bdca6905243cc22e62d7d36e7571ac37e9c5f1bc
박소정 <thwjd0235@naver.com>
2024-11-17 04:04:27 +0900

    feat: NavBar컴포넌트 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
 import { AppRouter } from "./routes/AppRouter";
+import { NavBar } from "./components/shared/NavBar/NavBar.jsx";
+import { useLocation } from "react-router-dom";
 import "./reset.css";
 import "./App.css";
 
 function App() {
-  return <AppRouter />;
+  const location = useLocation();
+  const showNavbar = ["/", "/feed", "/my"].includes(location.pathname);
+
+  return (
+    <div>
+      <AppRouter />
+      {showNavbar && <NavBar />}
+    </div>
+  );
 }
 
 export default App;

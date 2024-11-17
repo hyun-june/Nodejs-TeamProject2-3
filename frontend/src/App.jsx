17507c5daeedb252138f493477f7669fbe9b73fc
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-11-18 03:27:17 +0900

    feat: 기본 레이아웃 잡기

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,14 @@
 import { AppRouter } from "./routes/AppRouter";
-import { NavBar } from "./components/shared/NavBar/NavBar.jsx";
-import { useLocation } from "react-router-dom";
 import "./reset.css";
 import "./App.css";
-import { AppLayout } from "./components/Layout/AllLayout/AppLayout.jsx";
 
 function App() {
-  const location = useLocation();
-  const showNavbar = ["/", "/feed", "/my"].includes(location.pathname);
 
   return (
-    <div>
-      <AppLayout>
-        <AppRouter />
-        {showNavbar && <NavBar />}
-      </AppLayout>
-    </div>
+    <div id="container">
+          <div id="bottom-sheet"/>
+          <AppRouter />
+      </div>
   );
 }
 
--- frontend/src/components/Layout/AllLayout/AppLayout.jsx
+++
@@ -1,4 +0,0 @@
-import "./AppLayout.css";
-export const AppLayout = ({ children }) => {
-  return <div className="container">{children}</div>;
-};

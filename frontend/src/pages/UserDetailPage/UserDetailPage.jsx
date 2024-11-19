70f628be29c5dd0f57af8ef8aad7b9e64c2fcfde
Hyunjun Lee <leehyunjune11@gmail.com>
2024-11-19 15:24:14 +0900

    feat:userDetailPage에 사진 넣기 기능 추가, FeedPage 구조 변경, AuthLayout에서 Header삭제 및 그에 따른 페이지 별 css 수정

--- frontend/src/pages/UserDetailPage/UserDetailPage.jsx
+++ frontend/src/pages/UserDetailPage/UserDetailPage.jsx
@@ -1,40 +1,114 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { AuthInput } from "../../components/shared/AuthInput/AuthInput";
 import { AuthButton } from "../../components/shared/AuthButton/AuthButton";
+import { Header } from "../../components/shared/Header/Header";
+import { FaPlus } from "react-icons/fa6";
+import { IoCloseSharp } from "react-icons/io5";
 import "./css/UserDetailPage.css";
 
 export const UserDetailPage = () => {
   const { register, handleSubmit } = useForm();
+  const [profileImgUrl, setProfileImgUrl] = useState("");
+  const [profileImgfile, setProfileImgfile] = useState(null);
 
-  const onUserSubmit = (formData) => {
+  const onUserSubmit = async (formData) => {
     console.log("Form Data:", formData);
+    const profileInfoData = new FormData();
+    profileInfoData.append("profileImg", profileImgfile);
+
+    // try {
+    //   const response = await fetch("/profile-upload", {
+    //     method: "POST",
+    //     body: profileInfoData,
+    //   });
+    //   if (!response.ok) {
+    //     throw new Error("이미지 업로드 실패");
+    //   }
+    //   const data = await response.json();
+    //   console.log("서버 데이터", data);
+    // } catch (error) {
+    //   console.log("이미지 업로드 실패", error);
+    // }
+  };
+
+  const handleProfileDelete = () => {
+    setProfileImgUrl("");
+    setProfileImgfile(null);
+  };
+
+  const handleProfileChange = (e) => {
+    if (!e.target.files) return;
+    const file = e.target.files[0];
+    console.log("dd", file);
+    if (file) {
+      let profile = window.URL.createObjectURL(file);
+      setProfileImgUrl(profile);
+      setProfileImgfile(file);
+    }
   };
   return (
-    <div className="userDetailPage-Container">
-      <form onSubmit={handleSubmit(onUserSubmit)}>
-        <AuthInput id="nickname" title="닉네임" register={register} />
-        <section className="detail-first-section">
-          <AuthInput id="age" title="나이" type="number" register={register} />
-          <AuthInput id="height" title="키" type="number" register={register} />
-        </section>
-        <section className="detail-second-section">
-          <AuthInput
-            id="weight"
-            title="체중"
-            type="number"
-            register={register}
-          />
-          <AuthInput
-            id="purpose weight"
-            title="목표 체중"
-            type="number"
-            register={register}
-          />
-          <AuthButton type="submit" className="button-color_blue">
-            저장
-          </AuthButton>
-        </section>
-      </form>
-    </div>
+    <>
+      <Header title="개인정보 입력" />
+      <div className="userDetailPage-Container">
+        {profileImgUrl ? (
+          <div className="profilePreview">
+            <img src={profileImgUrl} />
+            <button onClick={handleProfileDelete}>
+              <IoCloseSharp className="profile-icon" />
+            </button>
+          </div>
+        ) : (
+          <section className="profile-Section">
+            <label htmlFor="profileImage">
+              <FaPlus className="profile-icon" />
+              <span>사진 넣기</span>
+            </label>
+            <input
+              type="file"
+              id="profileImage"
+              onChange={handleProfileChange}
+            />
+          </section>
+        )}
+        <form
+          onSubmit={handleSubmit(onUserSubmit)}
+          className="profileInfo-form"
+        >
+          <AuthInput id="nickname" title="닉네임" register={register} />
+          <section className="detail-first-section">
+            <AuthInput
+              id="age"
+              title="나이"
+              type="number"
+              register={register}
+            />
+            <AuthInput
+              id="height"
+              title="키"
+              type="number"
+              register={register}
+            />
+          </section>
+          <section className="detail-second-section">
+            <AuthInput
+              id="weight"
+              title="체중"
+              type="number"
+              register={register}
+            />
+            <AuthInput
+              id="purpose weight"
+              title="목표 체중"
+              type="number"
+              register={register}
+            />
+            <AuthButton type="submit" className="button-color_blue">
+              저장
+            </AuthButton>
+          </section>
+        </form>
+      </div>
+    </>
   );
 };

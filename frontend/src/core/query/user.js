b69680d7416a9499df9ce2329b86c754b6ca0cfe
박소정 <thwjd0235@naver.com>
2024-11-22 02:28:14 +0900

    feat: 다른 사람 정보 조회 query

--- frontend/src/core/query/user.js
+++ frontend/src/core/query/user.js
@@ -1,9 +1,16 @@
-import { getMyInfo } from "../api/user";
+import { getMyInfo, getOtherInfo } from "../api/user";
 import { useQuery } from "@tanstack/react-query";
 
 export const useGetMyInfo = () => {
   return useQuery({
     queryKey: ["me"],
     queryFn: getMyInfo,
+  });
+};
+
+export const useGetOtherInfo = (id) => {
+  return useQuery({
+    queryKey: ["user", id],
+    queryFn: () => getOtherInfo(id),
   });
 };

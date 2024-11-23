86a87b442d71300ad7dc4367bf84c9376d6e8948
박소정 <thwjd0235@naver.com>
2024-11-24 06:12:00 +0900

    feat: 처음 앱 실행할 때 cover컴포넌트 출력 후 클릭하면 없애기

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,31 @@
+import { useState, useEffect } from "react";
 import { AppRouter } from "./routes/AppRouter";
+import { Cover } from "./components/Main/Cover/Cover";
 import "./reset.css";
 import "./App.css";
 
 function App() {
+  const [isCoverOpen, setIsCoverOpen] = useState(false);
+
+  useEffect(() => {
+    if (!sessionStorage.getItem("isStart")) {
+      sessionStorage.setItem("isStart", "true");
+      setIsCoverOpen(true);
+    } else {
+      setIsCoverOpen(false);
+    }
+  }, []);
+
+  const handleCloseModal = () => {
+    sessionStorage.setItem("isStart", "false");
+    setIsCoverOpen(false);
+  };
 
   return (
     <div id="container">
-          <div id="bottom-sheet"/>
-          <AppRouter />
-      </div>
+      <div id="bottom-sheet" />
+      {isCoverOpen ? <Cover onClose={handleCloseModal} /> : <AppRouter />}
+    </div>
   );
 }
 
